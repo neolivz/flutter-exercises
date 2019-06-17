037858e050068f218a41e90e13af497314dad733
Jishnu Viswanath <github@jishnu.me>
2019-06-18 06:52:34 +1000

    feat: [INIT] initial commit

---
+++ my_stateless/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  var app = MaterialApp(
+    title: 'Flutter App',
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(),
+    ),
+  );
+  runApp(app);
+}
